46b93804dcf91021bafe00582311d61cff28ffb2
Konstantinos <tsoumascon@gmail.com>
2023-05-23 13:59:24 +0200

    add the cv (folds) file path

--- src/config.py
+++ src/config.py
@@ -1,7 +1,3 @@
-TRAINING_FILE = "../input/Train.csv"
-
-CLEANED_FILE = "../input/df_cleaned.csv"
-
 INPUT_FOLDER = "../input/"
 
 MODELS_FOLDER = "../models/"
@@ -9,3 +5,9 @@ MODELS_FOLDER = "../models/"
 SOURCE_FOLDER = "../src/"
 
 VISUALS_FOLDER = "../visualizations/"
+
+TRAINING_FILE = "../input/Train.csv"
+
+TRAINING_FOLDS_FILE = "../input/train_folds.csv"
+
+CLEANED_FILE = "../input/df_cleaned.csv"
