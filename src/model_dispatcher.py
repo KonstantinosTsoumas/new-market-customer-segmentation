ed91070a201789fb66e6caba0ad8a928f76b1479
Konstantinos <tsoumascon@gmail.com>
2023-06-10 20:07:47 +0200

    add functions to retrieve instances of various models based on their names

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -1,16 +1,35 @@
 from sklearn.linear_model import LogisticRegression
-from sklearn import ensemble
-from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.multiclass import OneVsRestClassifier
 
+def get_logistic_regression():
+    return LogisticRegression()
+
+def get_decision_tree_gini():
+    return DecisionTreeClassifier(criterion="gini")
+
+def get_decision_tree_entropy():
+    return DecisionTreeClassifier(criterion="entropy")
+
+def get_rf():
+    return RandomForestClassifier()
+
+def get_naive_bayes():
+    return GaussianNB()
+
+def get_one_vs_rest():
+    return OneVsRestClassifier(LogisticRegression())
+
+# Modify the model_dispatcher dictionary
 models = {
-    "logistic_regression": LogisticRegression(),
-    "decision_tree_gini": tree.DecisionTreeClassifier(criterion="gini"),
-    "decision_tree_entropy": tree.DecisionTreeClassifier(criterion="entropy"),
-    "rf": ensemble.RandomForestClassifier(),
-    "naive_bayes": GaussianNB(),
-    "one_vs_rest": OneVsRestClassifier(LogisticRegression())
+    "logistic_regression": get_logistic_regression,
+    "decision_tree_gini": get_decision_tree_gini,
+    "decision_tree_entropy": get_decision_tree_entropy,
+    "rf": get_rf,
+    "naive_bayes": get_naive_bayes,
+    "one_vs_rest": get_one_vs_rest
 }
 
 def get_model(model_name):
